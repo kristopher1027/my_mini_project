a8399c79cca629b1974fd9e60208179f7d556f4f
kristopher1027 <etzkristokency2@gmail.com>
2026-08-22 05:38:08 +0100

    my 1st mini project

---
+++ profile_card/main.py
@@ -0,0 +1,26 @@
+user_name = "Christopher okoh" 
+user_location = "Benue"
+user_occupation = "Graphic Designer"
+user_hobbies = "Music, Football"
+user_quote = "creativity is intelligence having fun."
+
+
+print("======== original profile ==========")
+print(f"Name:{user_name}")
+print(f"Occupation: {user_occupation}")
+print(f"Location: {user_location}")
+print(f"Hobbies: {user_hobbies}")
+print(f"Quote: {user_quote}")
+
+
+
+
+user_name = "Emmanuw okoh"
+user_location = "Markudi"
+
+print("===== Update profile =====")
+print(f"Name:{user_name}")
+print(f"Occupation: {user_occupation}")
+print(f"Location: {user_location}")
+print(f"Hobbies: {user_hobbies}")
+print(f"Quote: {user_quote}")
---
+++ profile_card/question.md
@@ -0,0 +1,2 @@
+Build a small profile card for a fictional user. Store at least five facts in variables and print a readable
+profile. Then update one fact and print the updated profile.
