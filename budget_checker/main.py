54e6a7f1f7804dbd68fbef168a72c74046fbb2fb
CHRISTOPHER OKOH <etzkristokency2@gmail.com>
2026-09-01 17:02:17 +0100

    Add budget checker functionality

---
+++ budget_checker/main.py
@@ -0,0 +1,11 @@
+## Budget Checker
+
+budget = 250
+total_purchase = 275.50
+
+difference = budget- total_purchase
+is_within_budget = total_purchase <= budget
+
+print(f"Over/Under by: {difference}")
+print(f"is within budget: {is_within_budget}")
+
